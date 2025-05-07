e3cf4dcec5508e5abdf3769473d0a31574c78df5
grigorii goncharov <grigoriy85@gmail.com>
2025-05-07 16:08:28 +0300

    Выполнение задания 2 согланос ТЗ

--- src/category.py
+++ src/category.py
@@ -6,8 +6,15 @@ class Category:
     name: str
     description: str
     products: list[Product]
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
         self.name = name
         self.description = description
         self.products = products
+        Category.category_count +=1
+        Category.product_count += len(products)
+
+
+
