4079e934a178a838b0e9ff8dd5075ed01aa23f25
grigorii goncharov <grigoriy85@gmail.com>
2025-05-07 16:45:26 +0300

    Тест Product

---
+++ tests/test_product.py
@@ -0,0 +1,22 @@
+from src.product import Product
+
+
+def test_product_unit(product_unit: Product) -> None:
+    """ Проверка имеющегося вывода продукта"""
+
+    assert product_unit.name == "Iphone 16PRO"
+    assert product_unit.description == "Smartphone"
+    assert product_unit.price == 107990
+    assert product_unit.quantity == 2
+
+def test_wrong_product_unit(product_unit: Product) -> None:
+    """ Проверка вывода отсутствующего продукта"""
+
+    assert not product_unit.name == "Xiaomi"
+    assert not product_unit.description == "Scooter"
+    assert not product_unit.price == 43000
+    assert not product_unit.quantity == 1
+
+
+
+
