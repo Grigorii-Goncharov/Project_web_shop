b61de7958e1bae05056b35b02c26c99f6784f540
grigorii goncharov <grigoriy85@gmail.com>
2025-05-10 21:07:53 +0300

    Add Main

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     print(category1.products)
     print(category1.product_count)
     product = []
-    new_product = Product.new_product(
+    new_product = Product.new_product(product,
         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5}, product)
+         "quantity": 5})
     print(new_product.name)
     print(new_product.description)
     print(new_product.price)
