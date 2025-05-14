a0bdb42282e871b141b7c0770a160125b9aa5cdc
grigorii goncharov <grigoriy85@gmail.com>
2025-05-14 17:52:16 +0300

    Задание 1 Для класса

--- src/category.py
+++ src/category.py
@@ -44,3 +44,7 @@ class Category:
         for product in self.__products:
             products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
         return products_str
+
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {len(self.__products)}"
+
