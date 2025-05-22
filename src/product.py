3303709e015bd428b0fe230a67ae3d09e7a7653a
grigorii goncharov <grigoriy85@gmail.com>
2025-05-22 20:23:28 +0300

    Создан класс mixin для отладки

--- src/product.py
+++ src/product.py
@@ -1,6 +1,7 @@
 from src.abstract_class import BaseProduct
+from src.print_mixin import PrintMixin
 
-class Product(BaseProduct):
+class Product(BaseProduct, PrintMixin):
     """Класс продукт"""
 
     name: str
@@ -13,6 +14,7 @@ class Product(BaseProduct):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     @classmethod
     def new_product(cls, products_list, products_dict):
