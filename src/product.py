9197641a17b12bc2bae6edd39219504b2061856e
grigorii goncharov <grigoriy85@gmail.com>
2025-05-26 18:06:26 +0300

    добавлен в метод инит класса продукт проверка на нулевое количество тоавра, которое нельзя добавить

--- src/product.py
+++ src/product.py
@@ -14,6 +14,8 @@ class Product(BaseProduct, PrintMixin):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        if self.quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         super().__init__()
 
     @classmethod
