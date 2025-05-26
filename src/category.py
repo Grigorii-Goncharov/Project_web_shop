fca48850314872030f59d7a911283fcbf24e242c
grigorii goncharov <grigoriy85@gmail.com>
2025-05-26 20:21:15 +0300

    класс исключения, который отвечает за обработку событий, когда в «атегорию» или «заказ»

--- src/category.py
+++ src/category.py
@@ -12,9 +12,21 @@ class Category(FormationProduct):
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
+        for product in products:
+            if not isinstance(product, Product):
+                raise TypeError("В категорию можно добавлять только объекты класса Product или его наследников")
         self.name = name
         self.description = description
         self.__products = products
+        try:
+            if self.__products == []:
+                raise TypeError("Передан пустой список")
+        except TypeError as e:
+            print(f"Вызвана ошибка: {e}")
+        else:
+            print("Товар добавлен")
+        finally:
+            print("Обработка добавления товара завершена")
 
         Category.category_count += 1
         Category.product_count += len(products)
@@ -55,7 +67,6 @@ class Category(FormationProduct):
     def products_list(self):
         return self.__products
 
-
     def middle_price(self):
         """
         Метод, который подсчитывает средний ценник всех товаров.
@@ -80,13 +91,35 @@ class Category(FormationProduct):
 
 class Order(FormationProduct):
     """Класс для оформления заказа"""
-
     def __init__(self, product, quantity):
-        self.product = product  # Ссылка на объект товара (например, Smartphone или LawnGrass)
+        self.product = product# Ссылка на объект товара (например, Smartphone или LawnGrass)
         self.quantity = quantity  # Количество купленного товара
-        if product.quantity < self.quantity:
-            raise ValueError("Количество заказа превышает количество на складе")
-        self.total_price = product.price * quantity  # получаем итоговую стоимость
+        try:
+            if product.quantity < self.quantity:
+                raise ValueError("Количество заказа превышает количество на складе")
+            elif self.quantity <= 0:
+                raise UndefinedObject("Количество товара должно быть больше нуля")
+        except ValueError as e:
+            print(f"Вызвана ошибка: {e}")
+        except UndefinedObject as e:
+            print(f"Ошибка: {e}")
+            self.total_price = 0  # Дефолтное значение
+        else:
+            print("Товар добавлен")
+        finally:
+            print("Обработка добавления товара завершена")
+
+        self.total_price = product.price * quantity  # Итоговая стоимость
+
 
     def __str__(self):
         return f'{self.product}, {self.quantity}, {self.total_price}'
+
+
+# if __name__ == "__main__":
+#     category = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [],
+#     )
+#     print(category)
---
+++ src/class_of_exception.py
@@ -0,0 +1,10 @@
+class ClassOfException(Exception):
+    """
+    Класс исключения, который отвечает за обработку событий, когда в «Категорию» или «Заказ»
+    добавляется товар с нулевым количеством.
+    """
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "Отсутствует значение"
+
+    def __str__(self):
+        return self.message
